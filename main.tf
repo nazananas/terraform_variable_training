275f6b97023f4f85e71d69cf2529a8ed98b383e5
Nazarii <nazananaskanal@gmail.com>
2025-04-07 21:43:14 +0300

    Fix errors

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami = var.ami
   instance_type = var.instance_type
-  key_name = aws_key_pair.existing_key.key_name
+  key_name = data.aws_key_pair.existing_key.key_name
   subnet_id = data.aws_subnet.existing.id
   vpc_security_group_ids = [data.aws_security_group.existing.id]
   tags = {
@@ -9,9 +9,20 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_key_pair" "existing_key" {
-  key_name   = "my-existing-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+data "aws_key_pair" "existing_key" {
+  key_name = "my-existing-key"
+}
+
+resource "aws_ebs_volume" "web_storage" {
+  availability_zone = data.aws_subnet.existing.availability_zone
+  size              = var.storage_size
+  type              = var.storage_type
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.web_storage.id
+  instance_id = aws_instance.web.id
 }
 
 data "aws_subnet" "existing" {
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+name = "TERRAFORM EC2"
+ami = "ami-0ecf75a98fe8519d7"
+instance_type = "t2.micro"
+subnet = "subnet-0ad2242284faf2e43"
+sg = "sg-00f66d9d4ada3399d"
+storage_size = 8
+storage_type = "gp3"
